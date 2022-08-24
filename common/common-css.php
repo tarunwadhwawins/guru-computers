b6f0f80e87ba86807872e9efe50dae997bd40b52
tarunwadhwawins <tarunwadhwawins@gmail.com>
2022-08-24 11:52:32 +0530

    merge changes

--- common/common-css.php
+++ common/common-css.php
@@ -3,10 +3,17 @@
 require_once(dirname(__DIR__).'/core/dbconnection.php');
 ?>
 <?php include_once(dirname(__DIR__).'/common/metatags.php'); ?>
-
+<?php
+$current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$current_page = str_replace($url, "", $current_url); 
+if($current_page == ""){
+  $current_page = "/";
+}
+?>
 <!--  -->
 <!-- Fav-icon -->
 <link rel="icon" href="<?php echo $url; ?>assets/images/favicon.png" type="image/x-icon">
+<link rel="canonical" href="<?php echo $url, $current_page; ?>" />
 <!--  -->
 <!-- Link -->
 <link rel="stylesheet" href="<?php echo $url; ?>assets/css/bootstrap.min.css">
--- common/metatags.php
+++ common/metatags.php
@@ -1,10 +1,4 @@
-<?php
-$current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$current_page = str_replace($url, "", $current_url); 
-if($current_page == ""){
-  $current_page = "/";
-}
-?>
+
 
 
 <title><?php echo $title ?></title>
@@ -13,4 +7,3 @@ if($current_page == ""){
 <meta name="description" content="<?php echo $description ?>">
 <meta name="keywords" content="<?php echo $keyword ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="canonical" href="<?php echo $url, $current_page; ?>" />
