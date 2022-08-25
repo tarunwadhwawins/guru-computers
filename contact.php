6417380065a5428bd8404e80aed75898ea8c4c15
tarunwadhwawins <tarunwadhwawins@gmail.com>
2022-08-25 12:11:40 +0530

    merge changes

--- contact.php
+++ contact.php
@@ -117,7 +117,8 @@
                                 </div>
                                 <div class="col-sm-12">
                                     <div class="contactField">
-                                        <input  data-validation="recaptcha" data-validation-recaptcha-sitekey="6LdmI44hAAAAAPLZHO9DaTyvHoP0e5mpRZsjEX0S">
+                                        <input data-validation="recaptcha"
+                                            data-validation-recaptcha-sitekey="6LdmI44hAAAAAPLZHO9DaTyvHoP0e5mpRZsjEX0S">
                                     </div>
                                 </div>
                                 <div class="col-sm-12">
@@ -127,14 +128,14 @@
                                 </div>
                         </form>
                     </div>
-
                 </div>
             </div>
         </div>
-    </section 
-    <section class="contactMap">    
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2609.1455708569288!2d-122.89315088453829!3d49.15984507931819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5485d909135b5a27%3A0x42caa7bde48e26b9!2sGuru+IT+Solutions+-+Macbook+and+PC+repairs!5e0!3m2!1sen!2sin!4v1539838338968"
-        width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+    </section>
+    <section class="contactMap">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2609.1455708569288!2d-122.89315088453829!3d49.15984507931819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5485d909135b5a27%3A0x42caa7bde48e26b9!2sGuru+IT+Solutions+-+Macbook+and+PC+repairs!5e0!3m2!1sen!2sin!4v1539838338968"
+            width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen=""></iframe>
     </section>
     <!-- footer -->
     <?php include_once('common/footer.php'); ?>
